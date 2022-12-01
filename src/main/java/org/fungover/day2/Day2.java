d8de66d38db3fe4c2c2dfadc92f1ab16d1bd711f
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-01 15:56:39 +0100

    Prepare day2

---
+++ src/main/java/org/fungover/day2/Day2.java
@@ -0,0 +1,9 @@
+package org.fungover.day2;
+
+public class Day2 {
+    public static void main(String[] args) {
+        //        String s = stringFromFile(resourceStringToPath("/day2/day2.txt"));
+        String s = """
+                """;
+    }
+}
