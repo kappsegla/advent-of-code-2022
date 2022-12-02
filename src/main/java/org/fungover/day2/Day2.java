927bdec5c72a235c27fc9057d75095f3d6acac4b
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-02 08:24:25 +0100

    Day2 solution with Map

--- src/main/java/org/fungover/day2/Day2.java
+++ src/main/java/org/fungover/day2/Day2.java
@@ -1,5 +1,8 @@
 package org.fungover.day2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.fungover.util.FileReader.resourceStringToPath;
 import static org.fungover.util.FileReader.stringFromFile;
 
@@ -20,6 +23,36 @@ public class Day2 {
         System.out.println(sum);  //13565
         sum = s.lines().mapToInt(Day2::calculateScoreToWin).sum();
         System.out.println(sum);  //12424
+
+        //Using lookup map
+        Map<String, Integer> codeToPoints = new HashMap<>() {{
+            put("A X", 4);
+            put("A Y", 8);
+            put("A Z", 3);
+            put("B X", 1);
+            put("B Y", 5);
+            put("B Z", 9);
+            put("C X", 7);
+            put("C Y", 2);
+            put("C Z", 6);
+        }};
+        sum = s.lines().mapToInt(codeToPoints::get).sum();
+        System.out.println(sum);
+
+        //Using lookup map
+        Map<String, Integer> codeToPointsStep2 = new HashMap<>() {{
+            put("A X", 3); //Loose
+            put("A Y", 4); //Draw
+            put("A Z", 8); //Win
+            put("B X", 1);
+            put("B Y", 5);
+            put("B Z", 9);
+            put("C X", 2);
+            put("C Y", 6);
+            put("C Z", 7);
+        }};
+        sum = s.lines().mapToInt(codeToPointsStep2::get).sum();
+        System.out.println(sum);
     }
 
     private static int calculateScoreToWin(String choices) {
@@ -31,23 +64,23 @@ public class Day2 {
     }
 
     private static int convertToNumberBestChoice(int op, String yc) {
-        if( yc.equals("X")  ) { //We should loose
-            if( op == ROCK)
+        if (yc.equals("X")) { //We should loose
+            if (op == ROCK)
                 return SCISSORS;
-            if( op == PAPER)
+            if (op == PAPER)
                 return ROCK;
-            if( op == SCISSORS)
+            if (op == SCISSORS)
                 return PAPER;
         }
-        if( yc.equals("Y")  ) { //We should draw
+        if (yc.equals("Y")) { //We should draw
             return op;
         }
-        if( yc.equals("Z")  ) { //We should win
-            if( op == ROCK)
+        if (yc.equals("Z")) { //We should win
+            if (op == ROCK)
                 return PAPER;
-            if( op == PAPER)
+            if (op == PAPER)
                 return SCISSORS;
-            if( op == SCISSORS)
+            if (op == SCISSORS)
                 return ROCK;
         }
         throw new RuntimeException();
@@ -62,22 +95,22 @@ public class Day2 {
     }
 
     private static int yourpoints(int op, int yp) {
-        if( op == yp) //Draw
+        if (op == yp) //Draw
             return 3;
-        if( op == ROCK && yp == PAPER) //Rock vs Paper
+        if (op == ROCK && yp == PAPER) //Rock vs Paper
             return 6;
-        if( op == PAPER && yp == SCISSORS) //Paper vs Scissors
+        if (op == PAPER && yp == SCISSORS) //Paper vs Scissors
             return 6;
-        if( op == SCISSORS && yp == ROCK) //Scissors vs Rock
+        if (op == SCISSORS && yp == ROCK) //Scissors vs Rock
             return 6;
         return 0;
     }
 
     private static int convertToNumber(String o) {
-        return switch (o){
-            case "A","X"->ROCK;
-            case "B","Y"->PAPER;
-            case "C","Z"->SCISSORS;
+        return switch (o) {
+            case "A", "X" -> ROCK;
+            case "B", "Y" -> PAPER;
+            case "C", "Z" -> SCISSORS;
             default -> throw new RuntimeException();
         };
     }
