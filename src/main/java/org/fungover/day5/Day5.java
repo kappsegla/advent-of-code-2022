04110a1bc6d924c6ce7bdde4c7828e667c3618bc
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-05 07:29:13 +0100

    Day 5 format code

--- src/main/java/org/fungover/day5/Day5.java
+++ src/main/java/org/fungover/day5/Day5.java
@@ -10,7 +10,7 @@ import static org.fungover.util.FileReader.stringFromFile;
 
 public class Day5 {
 
-    public static List<Deque<String>> stacks= new ArrayList<>();
+    public static List<Deque<String>> stacks = new ArrayList<>();
 
     public static void main(String[] args) {
         String s = stringFromFile(resourceStringToPath("/day5/day5.txt"));
@@ -30,33 +30,32 @@ public class Day5 {
         //Step1 ZSQVCCJLL
         fillStacks(parts[0]);
         parts[1].lines().forEach(Day5::crateMover9000);
-        stacks.forEach(stack-> System.out.print(stack.peek()));
+        stacks.forEach(stack -> System.out.print(stack.peek()));
         stacks.clear();
         System.out.println();
         //Step2  QZFJRWHGS
         fillStacks(parts[0]);
         parts[1].lines().forEach(Day5::crateMover9001);
-        stacks.forEach(stack-> System.out.print(stack.peek()));
+        stacks.forEach(stack -> System.out.print(stack.peek()));
     }
 
     private static void fillStacks(String part) {
-        for( var line : part.lines().toList()) {
-            if( !line.contains("["))
+        for (var line : part.lines().toList()) {
+            if (!line.contains("["))
                 break;
             int pos = 0;
-            while(pos*4 < line.length())
-            {
-                if(stacks.size() < pos + 1)
+            while (pos * 4 < line.length()) {
+                if (stacks.size() < pos + 1)
                     stacks.add(new ArrayDeque<>());
-                var crateName = String.valueOf(line.charAt(pos*4+1));
-                if( crateName.charAt(0) != ' ' )
+                var crateName = String.valueOf(line.charAt(pos * 4 + 1));
+                if (crateName.charAt(0) != ' ')
                     stacks.get(pos).addLast(crateName);
                 pos++;
             }
         }
     }
 
-    public static void crateMover9000(String instruction){
+    public static void crateMover9000(String instruction) {
         var i = instruction.split(" ");
         var count = Integer.parseInt(i[1]);
         var from = Integer.parseInt(i[3]) - 1;
@@ -67,7 +66,7 @@ public class Day5 {
         }
     }
 
-    public static void crateMover9001(String instruction){
+    public static void crateMover9001(String instruction) {
         var i = instruction.split(" ");
         var count = Integer.parseInt(i[1]);
         var from = Integer.parseInt(i[3]) - 1;
