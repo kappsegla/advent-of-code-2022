7a96a7a84881d0327a64da7098de16dfdf9f5328
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-06 20:44:12 +0100

    Prepare day7

---
+++ src/main/java/org/fungover/day7/Day7.java
@@ -0,0 +1,10 @@
+package org.fungover.day7;
+
+import static org.fungover.util.FileReader.resourceStringToPath;
+import static org.fungover.util.FileReader.stringFromFile;
+
+public class Day7 {
+    public static void main(String[] args) {
+        String s = stringFromFile(resourceStringToPath("/day7/day7.txt"));
+    }
+}
