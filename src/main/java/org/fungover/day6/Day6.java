7be80ca0201c2b3c084c578fe7bf84346df2068b
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-06 11:27:23 +0100

    Day6 Refactor

--- src/main/java/org/fungover/day6/Day6.java
+++ src/main/java/org/fungover/day6/Day6.java
@@ -9,6 +9,7 @@ import static org.fungover.util.FileReader.stringFromFile;
 public class Day6 {
 
     public static final int MESSAGE_MARKER_SIZE = 14;
+    public static final int PACKET_MARKER_SIZE = 4;
 
     public static void main(String[] args) {
         String s = stringFromFile(resourceStringToPath("/day6/day6.txt"));
@@ -18,17 +19,9 @@ public class Day6 {
     }
 
     public static int packetMarker(String input) {
-        for (int i = 0; i < input.length() - 3; i++) {
-            var c1 = input.charAt(i);
-            var c2 = input.charAt(i + 1);
-            var c3 = input.charAt(i + 2);
-            var c4 = input.charAt(i + 3);
-
-            if (c1 != c2 && c1 != c3 && c1 != c4 &&
-                    c2 != c3 && c2 != c4 && c3 != c4) {
-                        //Valid packet marker
-                        return i + 4;
-                    }
+        for (int i = 0; i <= input.length() - PACKET_MARKER_SIZE; i++) {
+            if( isUnique(input.substring(i, i + PACKET_MARKER_SIZE)))
+                return i + PACKET_MARKER_SIZE;
         }
         throw new RuntimeException("Invalid input");
     }
