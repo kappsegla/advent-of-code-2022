0535b5998b453d4444df1dfe3e08ac054505079c
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-13 20:29:00 +0100

    Day13 Step2

--- src/main/java/org/fungover/day13/Day13.java
+++ src/main/java/org/fungover/day13/Day13.java
@@ -1,9 +1,9 @@
 package org.fungover.day13;
 
+import com.google.gson.*;
+
 import java.lang.reflect.Array;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.fungover.util.FileReader.resourceStringToPath;
 import static org.fungover.util.FileReader.stringFromFile;
@@ -11,64 +11,71 @@ import static org.fungover.util.Strings.stringToListOfListOfStrings;
 
 public class Day13 {
     public static void main(String[] args) {
-        //String s = stringFromFile(resourceStringToPath("/day13/day13.txt"));
-        String s = """
-                [1,1,3,1,1]
-                [1,1,5,1,1]
-                                
-                [[1],[2,3,4]]
-                [[1],4]
-                                
-                [9]
-                [[8,7,6]]
-                                
-                [[4,4],4,4]
-                [[4,4],4,4,4]
-                                
-                [7,7,7,7]
-                [7,7,7]
-                                
-                []
-                [3]
-                                
-                [[[]]]
-                [[]]
-                                
-                [1,[2,[3,[4,[5,6,7]]]],8,9]
-                [1,[2,[3,[4,[5,6,0]]]],8,9]
-                """;
-
+        String s = stringFromFile(resourceStringToPath("/day13/day13.txt"));
+//        String s = """
+//                [1,1,3,1,1]
+//                [1,1,5,1,1]
+//
+//                [[1],[2,3,4]]
+//                [[1],4]
+//
+//                [9]
+//                [[8,7,6]]
+//
+//                [[4,4],4,4]
+//                [[4,4],4,4,4]
+//
+//                [7,7,7,7]
+//                [7,7,7]
+//
+//                []
+//                [3]
+//
+//                [[[]]]
+//                [[]]
+//
+//                [1,[2,[3,[4,[5,6,7]]]],8,9]
+//                [1,[2,[3,[4,[5,6,0]]]],8,9]
+//                """; //13 //140
+        //Step1
         var lines = stringToListOfListOfStrings(s);
-
         Set<Integer> rightOrderPairs = new HashSet<>();
         for (int i = 0; i < lines.size(); i++) {
             List<String> pair = lines.get(i);
-            if (isPairInRightOrder(pair.get(0), pair.get(1)))
+            if (compare(JsonParser.parseString(pair.get(0)), JsonParser.parseString(pair.get(1))) < 0)
                 rightOrderPairs.add(i + 1);
         }
         System.out.println(rightOrderPairs.stream().mapToInt(i -> i).sum());
+        //Step2
+        s += "\n[[2]]\n[[6]]";
+        lines = stringToListOfListOfStrings(s);
+        var sorted = lines.stream().flatMap(Collection::stream).map(JsonParser::parseString).sorted(Day13::compare).toList();
+        JsonElement divider1 = JsonParser.parseString("[[2]]");
+        JsonElement divider2 = JsonParser.parseString("[[6]]");
+        System.out.println( (sorted.indexOf(divider1)+1) * (sorted.indexOf(divider2)+1));
 
     }
 
-    private static boolean isPairInRightOrder(String a, String b) {
-        var left = a.replaceAll("[\\[\\]]", "").split(",");
-        var right = b.replaceAll("[\\[\\]]", "").split(",");
-
-        int c = 0;
-        while (c < left.length && c < right.length) {
-            if( left[0].isEmpty())
-                return true;
-            int v1 = Integer.parseInt(left[c]);
-            int v2 = Integer.parseInt(right[c]);
-
-            if (v1 > v2)
-                return false;
-            if (v1 < v2)
-                return true;
-            c++;
+    public static int compare(JsonElement first, JsonElement second) {
+        if (first instanceof JsonPrimitive firstInteger && second instanceof JsonArray) {
+            JsonArray array = new JsonArray();
+            array.add(firstInteger);
+            return compare(array, second);
+        } else if (first instanceof JsonArray && second instanceof JsonPrimitive secondInteger) {
+            JsonArray array = new JsonArray();
+            array.add(secondInteger);
+            return compare(first, array);
+        } else if (first instanceof JsonPrimitive firstInteger && second instanceof JsonPrimitive secondInteger) {
+            return Integer.compare(firstInteger.getAsInt(), secondInteger.getAsInt());
+        } else if (first instanceof JsonArray firstArray && second instanceof JsonArray secondArray) {
+            for (int i = 0; i < Math.min(firstArray.size(), secondArray.size()); i++) {
+                int result = compare(firstArray.get(i), secondArray.get(i));
+                if (result != 0) {
+                    return result;
+                }
+            }
+            return compare(new JsonPrimitive(firstArray.size()), new JsonPrimitive(secondArray.size()));
         }
-        if( left.length < right.length)
-            return true;
-        return false;
+        return 0;
     }
 }
