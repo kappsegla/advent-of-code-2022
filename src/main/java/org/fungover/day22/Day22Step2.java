cec81168f916891d29fbadcc2b1666857b0b7152
Martin Blomberg <martin.blomberg@outlook.com>
2022-12-22 23:12:27 +0100

    Day22 Step2 almost, probaly of by 1 somewhere.

--- src/main/java/org/fungover/day22/Day22Step2.java
+++ src/main/java/org/fungover/day22/Day22Step2.java
@@ -5,12 +5,7 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static org.fungover.day22.Day22Step2.DIR.*;
-import static org.fungover.day22.Day22Step2.SIDE.A;
-import static org.fungover.day22.Day22Step2.SIDE.B;
-import static org.fungover.day22.Day22Step2.SIDE.C;
-import static org.fungover.day22.Day22Step2.SIDE.D;
-import static org.fungover.day22.Day22Step2.SIDE.E;
-import static org.fungover.day22.Day22Step2.SIDE.F;
+import static org.fungover.day22.Day22Step2.SIDE.*;
 import static org.fungover.util.FileReader.resourceStringToPath;
 import static org.fungover.util.FileReader.stringFromFile;
 
@@ -69,17 +64,22 @@ public class Day22Step2 {
         }
         var currentPos = new PointAndDir(posX, posY, RIGHT);
 
-        BiFunction<PointAndDir, Grid, PointAndDir> nextPos = Day22Step2::nextPos;
-        //BiFunction<PointAndDir, Grid, PointAndDir> nextPos = Day22Step2::nextCubePos;
+        //BiFunction<PointAndDir, Grid, PointAndDir> nextPos = Day22Step2::nextPos;
+        BiFunction<PointAndDir, Grid, PointAndDir> nextPos = Day22Step2::nextCubePos;
 
         //Parse instructions
         var instructions = Arrays.stream(path.split("((?=\\D))|((?<=\\D))")).map(Day22Step2::toCommand).toList();
 
+        var next = currentPos;
         for (var command : instructions) {
             if (command instanceof StepCommand stepCommand)
                 for (int i = 0; i < stepCommand.steps; i++) {
                     // Get nextPos
-                    var next = nextPos.apply(currentPos, gridObj);
+                    if (sideOf(new PointAndDir(currentPos.x + currentPos.dir.point2D.x, currentPos.y + currentPos.dir.point2D.y, currentPos.dir)) != OUTSIDE)
+                        next = new PointAndDir(currentPos.x + currentPos.dir.point2D.x, currentPos.y + currentPos.dir.point2D.y, currentPos.dir);
+                    else //Wrap around
+                        next = nextPos.apply(currentPos, gridObj);
+                    //var next = nextPos.apply(currentPos, gridObj);
                     if (isWall(next, grid)) break;
                     currentPos = next;
                 }
@@ -103,94 +103,93 @@ public class Day22Step2 {
         return grid[pos.x()][pos.y()] == 2;
     }
 
-
     private static PointAndDir nextPos(PointAndDir currentPos, Grid gridObj) {
         var delta = currentPos.dir.point2D;
         var grid = gridObj.getGrid();
         var tryPoint = currentPos;
-        while(true) {
+        while (true) {
             tryPoint = new PointAndDir(tryPoint.x() + delta.x(), tryPoint.y() + delta.y(), tryPoint.dir);
             //Check if tryPoint is outside grid to the right, then wrap to the left
-            if( tryPoint.x() > grid.length-1)
-                tryPoint = new PointAndDir(0, tryPoint.y(),tryPoint.dir);
-            if( tryPoint.x() < 0)
-                tryPoint = new PointAndDir(grid.length - 1, tryPoint.y(),tryPoint.dir);
-            if( tryPoint.y() > grid[0].length-1)
-                tryPoint = new PointAndDir(tryPoint.x(), 0,tryPoint.dir);
-            if( tryPoint.y() < 0 )
-                tryPoint = new PointAndDir(tryPoint.x(), grid[0].length-1,tryPoint.dir);
-            if( grid[tryPoint.x()][tryPoint.y()] == 1) //Valid pos
+            if (tryPoint.x() > grid.length - 1)
+                tryPoint = new PointAndDir(0, tryPoint.y(), tryPoint.dir);
+            if (tryPoint.x() < 0)
+                tryPoint = new PointAndDir(grid.length - 1, tryPoint.y(), tryPoint.dir);
+            if (tryPoint.y() > grid[0].length - 1)
+                tryPoint = new PointAndDir(tryPoint.x(), 0, tryPoint.dir);
+            if (tryPoint.y() < 0)
+                tryPoint = new PointAndDir(tryPoint.x(), grid[0].length - 1, tryPoint.dir);
+            if (grid[tryPoint.x()][tryPoint.y()] == 1) //Valid pos
                 return tryPoint;
-            if( grid[tryPoint.x()][tryPoint.y()] == 2) //Wall, return for now
+            if (grid[tryPoint.x()][tryPoint.y()] == 2) //Wall, return for now
                 return tryPoint;
-            if( grid[tryPoint.x()][tryPoint.y()] == 0) //Don't go here, keep moving
+            if (grid[tryPoint.x()][tryPoint.y()] == 0) //Don't go here, keep moving
                 continue;
         }
     }
 
-    public static PointAndDir nextCubePos(PointAndDir curr,  Grid gridObj){
+    public static PointAndDir nextCubePos(PointAndDir curr, Grid gridObj) {
         var nextDir = curr.dir;
         var currDir = curr.dir;
         var currSide = sideOf(curr);
         var nextPos = new Point2D(curr.x, curr.y);
         if (currSide == A && currDir == UP) {
             nextDir = RIGHT;
-            nextPos = new Point2D(0, 3 * 50 + curr.x - 50); // nextSide = F
+            nextPos = new Point2D(150 + curr.y - 50, 0); // nextSide = F !
         } else if (currSide == A && currDir == LEFT) {
             nextDir = RIGHT;
-            nextPos = new Point2D(0, 2 * 50 + (50 - curr.y - 1)); // nextSide = E
+            nextPos = new Point2D(100 + (50 - curr.x - 1), 0); // nextSide = E !
         } else if (currSide == B && currDir == UP) {
             nextDir = UP;
-            nextPos = new Point2D(curr.x - 100, 199);// nextSide = F
+            nextPos = new Point2D(199, curr.y - 100);// nextSide = F
         } else if (currSide == B && currDir == RIGHT) {
             nextDir = LEFT;
-            nextPos = new Point2D(99, (50 - curr.y) + 2 * 50 - 1); // nextSide = D
+            nextPos = new Point2D((50 - curr.x) + 2 * 50 - 1, 99); // nextSide = D  ??
         } else if (currSide == B && currDir == DOWN) {
             nextDir = LEFT;
-            nextPos = new Point2D(99, 50 + (curr.x - 2 * 50)); // nextSide = C
+            nextPos = new Point2D(50 + (curr.y - 2 * 50), 99); // nextSide = C !
         } else if (currSide == C && currDir == RIGHT) {
             nextDir = UP;
-            nextPos = new Point2D((curr.y - 50) + 2 * 50, 49); // nextSide = B
+            nextPos = new Point2D(49, (curr.x - 50) + 2 * 50); // nextSide = B !
         } else if (currSide == C && currDir == LEFT) {
             nextDir = DOWN;
-            nextPos = new Point2D(curr.y - 50, 100); // nextSide = E
+            nextPos = new Point2D(100, curr.x - 50); // nextSide = E !
         } else if (currSide == E && currDir == LEFT) {
             nextDir = RIGHT;
-            nextPos = new Point2D(50, 50 - (curr.y - 2 * 50) - 1); // nextSide = A
+            nextPos = new Point2D(curr.x - 2 * 50, 50); // nextSide = A !!
         } else if (currSide == E && currDir == UP) {
             nextDir = RIGHT;
-            nextPos = new Point2D(50, 50 + curr.x); // nextSide = C
+            nextPos = new Point2D(50 + curr.y, 50); // nextSide = C !
         } else if (currSide == D && currDir == DOWN) {
             nextDir = LEFT;
-            nextPos = new Point2D(49, 3 * 50 + (curr.x - 50)); // nextSide = F
+            nextPos = new Point2D(3 * 50 + (curr.y - 50), 49); // nextSide = F !
         } else if (currSide == D && currDir == RIGHT) {
             nextDir = LEFT;
-            nextPos = new Point2D(149, 50 - (curr.y - 50 * 2) - 1); // nextSide = B
+            nextPos = new Point2D(50 - (curr.x - 50 * 2) - 1, 149); // nextSide = B !??
         } else if (currSide == F && currDir == RIGHT) {
             nextDir = UP;
-            nextPos = new Point2D((curr.y - 3 * 50) + 50, 149); // nextSide = D
+            nextPos = new Point2D(149, (curr.x - 3 * 50) + 50); // nextSide = D !
         } else if (currSide == F && currDir == LEFT) {
             nextDir = DOWN;
-            nextPos = new Point2D(50 + (curr.y - 3 * 50), 0); // nextSide = A
+            nextPos = new Point2D(0, 50 + (curr.x - 3 * 50)); // nextSide = A !
         } else if (currSide == F && currDir == DOWN) {
             nextDir = DOWN;
-            nextPos = new Point2D(curr.x + 100, 0); // nextSide = B
+            nextPos = new Point2D(0, curr.y + 100); // nextSide = B !
         }
         return new PointAndDir(nextPos.x, nextPos.y, nextDir);
     }
 
     private static SIDE sideOf(PointAndDir pos) {
-            if (pos.x >= 50 && pos.x <= 99 && pos.y >= 0 && pos.y <= 49) return A;
-            if (pos.x >= 100 && pos.x <= 149 && pos.y >= 0 && pos.y <= 49) return B;
-            if (pos.x >= 50 && pos.x <= 99 && pos.y >= 50 && pos.y <= 99) return C;
-            if (pos.x >= 50 && pos.x <= 99 && pos.y >= 100 && pos.y <= 149) return D;
-            if (pos.x >= 0 && pos.x <= 49 && pos.y >= 100 && pos.y <= 149) return E;
-            if (pos.x >= 0 && pos.x <= 49 && pos.y >= 150 && pos.y <= 199) return F;
-        throw new IllegalStateException();
+        if (pos.x >= 0 && pos.x <= 49 && pos.y >= 50 && pos.y <= 99) return A;
+        if (pos.x >= 0 && pos.x <= 49 && pos.y >= 100 && pos.y <= 149) return B;
+        if (pos.x >= 50 && pos.x <= 99 && pos.y >= 50 && pos.y <= 99) return C;
+        if (pos.x >= 100 && pos.x <= 149 && pos.y >= 50 && pos.y <= 99) return D;
+        if (pos.x >= 100 && pos.x <= 149 && pos.y >= 0 && pos.y <= 49) return E;
+        if (pos.x >= 150 && pos.x <= 199 && pos.y >= 0 && pos.y <= 49) return F;
+        return OUTSIDE;
     }
 
     enum SIDE {
-        A, B, C, D, E, F
+        A, B, C, D, E, F, OUTSIDE
     }
 
     enum DIR {
@@ -210,14 +209,14 @@ public class Day22Step2 {
         }
 
         public DIR right() {
-            return values()[modulus(this.ordinal() + 1 , values().length)];
+            return values()[modulus(this.ordinal() + 1, values().length)];
         }
 
         public DIR left() {
-            return values()[modulus(this.ordinal() - 1 , values().length)];
+            return values()[modulus(this.ordinal() - 1, values().length)];
         }
 
-        private int modulus(int x, int y){
+        private int modulus(int x, int y) {
             return (Math.floorMod(x, y) + Math.abs(y)) % Math.abs(y);
         }
     }
